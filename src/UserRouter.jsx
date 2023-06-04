39e086ba709923972e218164889bf834c5fe7d4f
ertyhd <orionship@protonmail.com>
2023-06-05 02:28:32 +0300

    add loader

--- src/UserRouter.jsx
+++ src/UserRouter.jsx
@@ -1,5 +1,9 @@
-import { lazy } from 'react';
+import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
+
+// import PrivateRote from 'modules/PrivateRote/PrivateRote';
+// import PublicRote from 'modules/PublicRote/PublicRote';
+// import HeaderWraper from 'components/HeaderWraper/HeaderWraper';
 
 const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
 const TweetsPage = lazy(() => import('./pages/TweetsPage/TweetsPage'));
@@ -7,11 +11,13 @@ const NotFoundPage = lazy(() => import('./pages/NotFoundPage/NotFoundPage'));
 
 const UserRoutes = () => {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/tweets" element={<TweetsPage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+    <Suspense>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/tweets" element={<TweetsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Suspense>
   );
 };
 
